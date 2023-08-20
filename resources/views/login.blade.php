95d266ad07837aa378d4af8c68fe6ca093730f46
Chu Seng <yongchuseng@gmail.com>
2023-08-20 09:55:29 +0800

    .

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,7 +15,7 @@
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
             <img class="mx-auto h-[120px] w-[120px] rounded-full" src="{{ URL::to('/assets/img/5_skull_momney.jpg') }}"
                     alt="Logo">
-            <h2 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in</h2>
+            <h2 class="mt-1 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to track your expenses!</h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
